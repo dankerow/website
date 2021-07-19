9add74d20076b473aa2480f6bc209e8f6d87a7f3
dankerow <78473524+dankerow@users.noreply.github.com>
2021-07-19 08:15:43 -0700

    📘 (locales) Added English translations

---
+++ src/locales/en.js
@@ -0,0 +1,30 @@
+export default {
+	shortDescription: 'A junior web developer, looking for more challenges.',
+	hero: {
+		headingOne: 'Hi, my name is',
+		shortDescription: 'A junior web developer living in Canada, currently enrolled at Columbia College, completing an Associate Degree in Computer Science.'
+	},
+	about: {
+		index: 'About',
+		paragraphOne: [
+			'Hi, I\'m Dan, a junior web developer living in Canada. I like to express my creativity in many ways, such as creating websites and graphic designs. I started to get interested in web development and graphic design in late 2015. Since then I have continued to learn and practice each of these areas.',
+			'I am by nature very curious, I like to discover new things, and I easily integrate into a team.',
+			'I speak several languages such as French, English, and Italian, and I am learning Spanish which I already understand quite well.'
+		],
+		skillsCard: {
+			cardTitle: 'Skills'
+		}
+	},
+	experience: {
+		index: 'Experience',
+		jobs: [
+			{
+				organizationName: 'Work with me',
+				description: 'I don\'t have any work experience yet, that\'s why I am looking for opportunities to apply the skills I have acquired on my own over the last 3 years.'
+			}
+		]
+	},
+	projects: {
+		index: 'Projects'
+	}
+}
