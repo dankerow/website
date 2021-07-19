ad848dea12c5234817bd12a909285b71ffbc7ce3
dankerow <78473524+dankerow@users.noreply.github.com>
2021-07-19 08:15:53 -0700

    📘 (locales) Added French translations

---
+++ src/locales/fr.js
@@ -0,0 +1,30 @@
+export default {
+	shortDescription: 'Un développeur web en herbe, à la recherche de nouveaux défis.',
+	hero: {
+		headingOne: 'Salut, je m\'appelle',
+		shortDescription: 'Développeur web en herbe vivant au Canada, actuellement étudiant à Columbia College pour obtenir un diplôme d\'associé en informatique.'
+	},
+	about: {
+		index: 'À propos',
+		paragraphOne: [
+			'Bonjour, je suis Dan, un développeur web en herbe vivant au Canada. J\'aime exprimer ma créativité de plusieurs façons, notamment en créant des sites web et des designs graphiques. J\'ai commencé à m\'intéresser au développement web et au graphisme fin 2015. Depuis, j\'ai continué à apprendre et à pratiquer chacun de ces domaines.',
+			'Je suis de nature très curieuse, j\'aime découvrir de nouvelles choses, et je m\'intègre facilement dans une équipe.',
+			'Je parle plusieurs langues tel que le français, l\'anglais et l\'italien, et j\'apprends l\'espagnol que je comprends déjà très bien.'
+		],
+		skillsCard: {
+			cardTitle: 'Compétences'
+		}
+	},
+	experience: {
+		index: 'Expérience',
+		jobs: [
+			{
+				organizationName: 'Work with me',
+				description: 'Je n\'ai pas encore d\'expérience professionnelle, c\'est pourquoi je cherche des occasions d\'appliquer les compétences que j\'ai acquises par moi-même au cours des trois dernières années.'
+			}
+		]
+	},
+	projects: {
+		index: 'Projets'
+	}
+}
