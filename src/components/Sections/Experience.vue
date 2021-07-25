4a8bd7a12dfb1fb144d48d3b8e7c09031c63a392
dankerow <78473524+dankerow@users.noreply.github.com>
2021-07-25 11:00:09 -0700

    📦 (components/sections/experience) Partial internationalization support

--- src/components/Sections/Experience.vue
+++ src/components/Sections/Experience.vue
@@ -3,26 +3,87 @@
 		<div class="container">
 			<div class="mb-5">
 				<h1 class="section-heading">
-					<span class="highlight">E</span><span class="highlight inverse">XP</span><span class="highlight">ERIENCE</span>.
+					<span class="highlight">E</span><span class="highlight inverse">XP</span><span class="highlight">ERIENCE</span>
 				</h1>
 			</div>
 
-			<div class="row row-cols-2">
-				<div class="col" />
+			<div class="row gy-2 row-cols-1 row-cols-sm-2">
+				<div class="col col-lg-4">
+					<div class="experience-pills float-end">
+						<div id="experience-pills-tab" class="nav flex-column nav-pills" role="tablist" aria-orientation="vertical">
+							<button
+								v-for="(work, index) in $t('experience.jobs')"
+								:id="`experience-pills-${index}-tab`"
+								:key="index"
+								class="nav-link active"
+								data-bs-toggle="pill"
+								:data-bs-target="`#experience-pills-${index}`"
+								type="button"
+								role="tab"
+								:aria-controls="`experience-pills-${index}`"
+								aria-selected="true"
+							>
+								{{ work.organizationName }}
+							</button>
+						</div>
+					</div>
+				</div>
 				<div class="col">
-					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eu neque lobortis, consectetur lectus a, porttitor lorem. Cras vestibulum dignissim tristique. Donec eu auctor est, dictum fermentum mi. Nullam pharetra nunc id rhoncus congue. Sed molestie, nunc venenatis laoreet sagittis, purus urna dignissim felis, nec tempor orci massa ac quam. Sed euismod est eget est pharetra, vel euismod erat facilisis. Donec ac lacinia massa, non hendrerit eros. Nam pellentesque purus at elit ornare consectetur. Phasellus vitae mi quis risus interdum egestas sed ut augue. Curabitur commodo maximus nisi vitae egestas. Praesent in posuere ipsum.
-					</p>
-
-					<p>
-						Sed eu euismod tellus. Ut congue, magna non rutrum malesuada, nulla nisl finibus libero, id sodales ligula justo non augue. Suspendisse porta arcu aliquam dictum sagittis. Praesent semper in sem at efficitur. Nullam nec enim ultrices, viverra odio at, tincidunt mi. Donec eget nisi quam. Aliquam magna mi, mollis sit amet fringilla at, lacinia ac sem. Duis laoreet lorem non eleifend maximus. Nulla facilisi.
-					</p>
-
-					<p>
-						Etiam posuere dignissim ligula in rhoncus. Suspendisse potenti. Morbi efficitur arcu ac libero porta varius. Nullam euismod rutrum porttitor. Aenean at nisi quis ligula scelerisque blandit eu id nibh. Mauris non sollicitudin nibh. Ut laoreet nulla nunc. Ut quis dolor sit amet ligula eleifend tincidunt. Duis iaculis sem mauris, vitae placerat mauris egestas faucibus.
-					</p>
+					<div id="experience-pills-tabContent" class="tab-content">
+						<div
+							v-for="(work, index) in $t('experience.jobs')"
+							:id="`experience-pills-${index}`"
+							:key="index"
+							class="tab-pane fade show active"
+							role="tabpanel"
+							:aria-labelledby="`experience-pills-${index}-tab`"
+						>
+							<p>
+								{{ work.description }}
+							</p>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
 	</section>
 </template>
+
+<style lang="scss" scoped>
+.experience-pills {
+	width: 45%;
+}
+
+@media (max-width: 768px) {
+	.experience-pills {
+		width: 100%;
+	}
+}
+
+.nav {
+	border-right: 2px solid rgb(46, 52, 57);
+	counter-reset: nav-link;
+}
+
+.nav-link {
+	border-left: 3px solid rgb(28, 33, 37);
+	border-radius: 0;
+	padding: 0.475rem 0;
+
+	&:before {
+		counter-increment: nav-link;
+		content: "0" counter(nav-link) ".";
+		color: rgb(195, 212, 232);
+		font-family: "Roboto Mono", sans-serif;
+	}
+
+	&.active {
+		background-color: rgba(51, 61, 73, 0.35);
+		border-left: 3px solid rgb(195, 212, 232);
+	}
+}
+
+.tab-content {
+	padding-left: 1rem;
+}
+</style>
