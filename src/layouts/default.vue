4fdd517aa94de84d8aeeebdc661a9d3023a42f09
dankerow <78473524+dankerow@users.noreply.github.com>
2021-07-25 11:05:10 -0700

    📦 (layouts/default) SEO changes related & Added a cookie banner

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -5,5 +5,30 @@
 		<Nuxt role="main" />
 
 		<Footer />
+
+		<CookieBanner />
 	</div>
 </template>
+
+<script>
+export default {
+	head() {
+		return {
+			...this.$nuxtI18nHead({ addSeoAttributes: true }),
+			meta: [
+				{
+					hid: 'description',
+					name: 'description',
+					content: this.$i18n.t('shortDescription')
+				},
+				{
+					hid: 'og:description',
+					name: 'og:description',
+					property: 'og:description',
+					content: this.$i18n.t('shortDescription')
+				}
+			]
+		}
+	}
+}
+</script>
