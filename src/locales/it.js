8be16d63dc4447307338e72539a32e51d7d1c32d
dankerow <78473524+dankerow@users.noreply.github.com>
2021-07-25 10:56:19 -0700

    ✨ (locales) Added Italian language

---
+++ src/locales/it.js
@@ -0,0 +1,31 @@
+export default {
+	shortDescription: 'Uno sviluppatore web junior, in cerca di nuove avventure.',
+	hero: {
+		headingOne: 'Ciao, mi chiamo',
+		shortDescription: 'Uno sviluppatore web junior residente in Canada, attualmente studente in Columbia College, che sta completando una laurea associata in scienze informatiche.'
+	},
+	about: {
+		index: 'Chi sono',
+		paragraphOne: [
+			'Ciao, mi chiamo Dan e sono uno sviluppatore web junior dal Canada. Mi piace esprimere la mia creatività in una varietà di modi, come ad esempio attraverso lo sviluppo di siti web e design grafico. Alla fine del 2015, ho sviluppato un interesse per lo sviluppo web e il design grafico. Da allora, ho continuato a imparare e praticare in ciascuna di queste aree.',
+			'Naturalmente curioso e mi piace imparare cose nuove, mi integro bene con gli altri.',
+			'Fluente in diverse lingue, tra cui francese, inglese e italiano, e attualmente sto imparando lo spagnolo, che già comprendo molto bene.'
+		],
+		skillsCard: {
+			cardTitle: 'Competenze'
+		}
+	},
+	experience: {
+		index: 'Esperienza',
+		jobs: [
+			{
+				organizationName: 'Lavora con me',
+				description: 'Non ho ancora nessuna esperienza professionale, è per questo che sto cercando opportunità per applicare le competenze che ho acquisito per conto mio negli ultimi 3 anni.'
+			}
+		]
+	},
+	projects: {
+		index: 'Progetti',
+		tempMessage: 'Sto ancora cucinando, si prega di tornare più tardi. Dovrebbe essere pronto presto. 😉'
+	}
+}
