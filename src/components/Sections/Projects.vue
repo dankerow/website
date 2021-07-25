bf15d7ac1dd8870011e4a68f7d798d8e6df634b3
dankerow <78473524+dankerow@users.noreply.github.com>
2021-07-25 11:01:34 -0700

    📦 (components/sections/projects) Partial internationalization support

--- src/components/Sections/Projects.vue
+++ src/components/Sections/Projects.vue
@@ -1,5 +1,5 @@
 <template>
-	<section id="projects" class="py-12" style="background: #191e21;">
+	<section id="projects" class="py-12">
 		<div class="container">
 			<div class="mb-5">
 				<h1 class="section-heading">
@@ -7,21 +7,19 @@
 				</h1>
 			</div>
 
-			<div class="row row-cols-2">
-				<div class="col">
+			<div class="row">
+				<div class="col text-center">
 					<p>
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eu neque lobortis, consectetur lectus a, porttitor lorem. Cras vestibulum dignissim tristique. Donec eu auctor est, dictum fermentum mi. Nullam pharetra nunc id rhoncus congue. Sed molestie, nunc venenatis laoreet sagittis, purus urna dignissim felis, nec tempor orci massa ac quam. Sed euismod est eget est pharetra, vel euismod erat facilisis. Donec ac lacinia massa, non hendrerit eros. Nam pellentesque purus at elit ornare consectetur. Phasellus vitae mi quis risus interdum egestas sed ut augue. Curabitur commodo maximus nisi vitae egestas. Praesent in posuere ipsum.
-					</p>
-
-					<p>
-						Sed eu euismod tellus. Ut congue, magna non rutrum malesuada, nulla nisl finibus libero, id sodales ligula justo non augue. Suspendisse porta arcu aliquam dictum sagittis. Praesent semper in sem at efficitur. Nullam nec enim ultrices, viverra odio at, tincidunt mi. Donec eget nisi quam. Aliquam magna mi, mollis sit amet fringilla at, lacinia ac sem. Duis laoreet lorem non eleifend maximus. Nulla facilisi.
-					</p>
-
-					<p>
-						Etiam posuere dignissim ligula in rhoncus. Suspendisse potenti. Morbi efficitur arcu ac libero porta varius. Nullam euismod rutrum porttitor. Aenean at nisi quis ligula scelerisque blandit eu id nibh. Mauris non sollicitudin nibh. Ut laoreet nulla nunc. Ut quis dolor sit amet ligula eleifend tincidunt. Duis iaculis sem mauris, vitae placerat mauris egestas faucibus.
+						{{ $t('projects.tempMessage') }}
 					</p>
 				</div>
 			</div>
 		</div>
 	</section>
 </template>
+
+<style lang="scss" scoped>
+#projects {
+	background: #191e21;
+}
+</style>
