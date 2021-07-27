1393799ab9f1a95d131129975250483af5c94f3a
dankerow <78473524+dankerow@users.noreply.github.com>
2021-07-27 15:47:13 -0700

    📘 (locales) Updated French language

--- src/locales/fr.js
+++ src/locales/fr.js
@@ -1,8 +1,9 @@
 export default {
 	shortDescription: 'Un développeur web en herbe, à la recherche de nouveaux défis.',
 	hero: {
-		headingOne: 'Salut, je m\'appelle',
-		shortDescription: 'Développeur web en herbe vivant au Canada, actuellement étudiant à Columbia College pour obtenir un diplôme d\'associé en informatique.'
+		headingOne: 'Salutations!',
+		shortDescription: 'Développeur web en herbe vivant au Canada, actuellement étudiant à {college} pour obtenir un diplôme d\'associé en informatique.',
+		college: 'Columbia College'
 	},
 	about: {
 		index: 'À propos',
@@ -19,7 +20,22 @@ export default {
 		index: 'Expérience',
 		jobs: [
 			{
-				organizationName: 'Work with me',
+				organizationName: 'UNICEF',
+				jobTitle: 'Développeur web',
+				jobType: 'Stage',
+				timePeriod: {
+					start: 'June 2020',
+					end: 'March 2021'
+				},
+				description: [
+					'Réalisation d\'une application web pour gérer les stocks de l\'entreprise en utilisant Express.js, Bootstrap et MongoDB.',
+					'Efficacité accrue pour un effectif de 5 personnes du département informatique ; transfert de MS Access à une application web disponible sur des appareils mobiles et informatiques.'
+				]
+			},
+			{
+				organizationName: 'Travaillez avec moi',
+				jobTitle: 'Travaillez avec moi',
+				jobType: 'Waiting for you',
 				description: 'Je n\'ai pas encore d\'expérience professionnelle, c\'est pourquoi je cherche des occasions d\'appliquer les compétences que j\'ai acquises par moi-même au cours des trois dernières années.'
 			}
 		]
