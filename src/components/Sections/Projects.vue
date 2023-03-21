ad883b7bf7ae6dcdd70692f57f9545c13b61a382
Dan <78473524+dankerow@users.noreply.github.com>
2023-03-21 14:19:59 -0700

    feat: Added my GitHub repositories

--- src/components/Sections/Projects.vue
+++ src/components/Sections/Projects.vue
@@ -1,25 +1,72 @@
+<script setup lang="ts">
+import { Ref } from 'vue'
+
+const repos: Ref<object[]> = ref([])
+const { data: repositoriesData, pending } = await useLazyFetch('https://api.github.com/users/dankerow/repos?per_page=100', { immediate: process.client, default: () => shallowRef() })
+watch(repositoriesData, (newData) => {
+	repos.value = newData
+})
+
+repos.value = repos.value?.sort((a, b) => b?.stargazers_count - a?.stargazers_count)
+</script>
+
 <template>
-	<section id="projects" class="py-12">
+	<section id="projects" class="py-10">
 		<div class="container">
-			<div class="mb-5">
-				<h1 class="section-heading">
-					<span class="highlight">PRO</span><span class="highlight inverse">J</span><span class="highlight">ECTS</span>
-				</h1>
+			<h1 class="h3 section-heading mb-5">
+				Projects
+			</h1>
+
+			<div v-if="pending" class="row row-cols-1 row-cols-lg-3 gy-4">
+				<div v-for="i in 6" :key="`skeleton-${i}`" class="col">
+					<div class="card shadow-sm" aria-hidden="true">
+						<div class="card-body placeholder-glow min-vh-50">
+							<h4 class="card-title text-truncate mb-4">
+								<span class="rounded placeholder col-1 me-2" />
+								<span class="rounded placeholder col-2" />
+								<span class="mx-1">/</span>
+								<span class="rounded placeholder col-3" />
+							</h4>
+
+							<p class="card-text">
+								<span class="rounded placeholder placeholder-sm col-7 my-2 d-block" />
+								<span class="rounded placeholder placeholder-sm col-4 my-2 d-block" />
+								<span class="rounded placeholder placeholder-sm col-8 my-2 d-block" />
+							</p>
+						</div>
+
+						<div class="card-footer placeholder-glow">
+							<span class="rounded placeholder col-1" />
+						</div>
+					</div>
+				</div>
 			</div>
 
-			<div class="row">
-				<div class="col text-center">
-					<p>
-						{{ $t('projects.tempMessage') }}
-					</p>
+			<div v-else>
+				<div class="row row-cols-1 row-cols-lg-3 gy-4">
+					<div
+						v-for="(repo, index) in repos"
+						:key="`repo-${index}`"
+						class="col"
+					>
+						<a
+							:href="repo.html_url"
+							target="blank"
+							rel="noreferrer"
+						>
+							<CardRepository
+								:name="repo.name"
+								:language="repo.language"
+								:stars="repo.stargazers_count"
+								:description="repo.description"
+								:license="repo.license && repo.license.spdx_id"
+								:top="index === 0"
+								class="h-100"
+							/>
+						</a>
+					</div>
 				</div>
 			</div>
 		</div>
 	</section>
 </template>
-
-<style lang="scss" scoped>
-#projects {
-	background: #191e21;
-}
-</style>
