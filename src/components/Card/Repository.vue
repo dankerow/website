9c68c666c12fee35ee3522898fb8e133192025a2
Dan <78473524+dankerow@users.noreply.github.com>
2023-03-21 14:21:26 -0700

    feat: Added Repository card component

---
+++ src/components/Card/Repository.vue
@@ -0,0 +1,84 @@
+<script setup lang="ts">
+defineProps<{
+	name: string
+	language?: string
+	stars: string|number
+	description?: string
+}>()
+
+const getIconName = (language: string) => {
+	switch (language) {
+	case 'javascript':
+		return 'cib:javascript'
+	case 'typescript':
+		return 'cib:typescript'
+	case 'html':
+		return 'cib:html-5'
+	case 'css':
+		return 'cib:css-3'
+	case 'php':
+		return 'cib:php'
+	case 'python':
+		return 'cib:python'
+	case 'c#':
+		return 'cib:c-sharp'
+	case 'c++':
+		return 'cib:c-plusplus'
+	case 'c':
+		return 'cib:c'
+	case 'java':
+		return 'cib:java'
+	case 'ruby':
+		return 'cib:ruby'
+	case 'shell':
+		return 'cib:shell'
+	case 'go':
+		return 'cib:go'
+
+	default:
+		return 'cib:github'
+	}
+}
+</script>
+
+<template>
+	<div class="card shadow-sm">
+		<div class="card-body min-vh-50">
+			<h4 class="card-title text-truncate mb-4">
+				<Icon :name="getIconName(language?.toLowerCase())" class="align-middle d-inline-block me-2 text-gray-100" />
+				<span class="align-middle d-inline-block text-white">dankerow/</span>
+				<span class="align-middle d-inline-block">{{ name }}</span>
+
+				<Icon name="material-symbols:open-in-new" class="open-new float-end" />
+			</h4>
+
+			<p class="card-text text-gray-300">
+				{{ description || $t('projects.project.noDescription') }}
+			</p>
+		</div>
+
+		<div class="card-footer d-flex align-items-center justify-content-between text-muted">
+			<div class="fs-5">
+				<Icon name="ph:star-duotone" class="align-middle d-inline-block me-1" />
+				<span class="align-middle d-inline-block">
+					{{ stars }}
+				</span>
+			</div>
+		</div>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+.card {
+	.open-new {
+		opacity: 0;
+		transition: opacity 0.3s ease-in-out;
+	}
+
+	&:hover {
+		.open-new {
+			opacity: 1;
+		}
+	}
+}
+</style>
