d9772151390e847e1bc79ad2ef543fa01887687c
Dan <78473524+dankerow@users.noreply.github.com>
2023-03-21 14:08:31 -0700

    style: Reworked section design

--- src/components/Sections/About.vue
+++ src/components/Sections/About.vue
@@ -1,13 +1,9 @@
 <template>
-	<section id="about" class="py-12">
+	<section id="about" class="py-10">
 		<div class="container">
-			<div class="mb-5">
-				<div class="col">
-					<h1 class="section-heading">
-						<span class="highlight">ABO</span><span class="highlight inverse">U</span><span class="highlight">T</span> Me
-					</h1>
-				</div>
-			</div>
+			<h1 class="h3 section-heading mb-5">
+				About Me
+			</h1>
 
 			<div class="row gy-5 row-cols-1 row-cols-lg-2">
 				<div class="col">
@@ -26,7 +22,7 @@
 				</div>
 
 				<div class="col">
-					<div class="card skills-card mx-auto me-lg-0">
+					<div class="card skills-card h-100 shadow-sm mx-auto me-lg-0">
 						<div class="card-header">
 							<h1 class="card-title text-center text-uppercase mb-0">
 								{{ $t('about.skillsCard.cardTitle') }}
@@ -63,7 +59,7 @@
 									:aria-labelledby="`skills-pills-${index}-tab`"
 								>
 									<div v-for="skill in category.skills" :key="`${$rt(category.name)}-${$rt(skill.title)}`" class="skill-category">
-										<h2 class="skill-category-title">
+										<h2 class="skill-category-title text-white text-uppercase">
 											{{ $rt(skill.title) }}:
 										</h2>
 
@@ -82,16 +78,11 @@
 </template>
 
 <style lang="scss" scoped>
-#about {
-   background: rgb(25, 30, 33);
-}
-
 .skills-card {
-	height:100%;
 	min-height: 340px;
 
 	.card-sub-header {
-		background-color: rgb(22, 26, 30);
+		background-color: rgba(24, 24, 24, 0.1);
 	}
 
 	.card-title {
@@ -104,16 +95,13 @@
 .skill-category {
 	.skill-category-title {
 		border-radius: .15rem;
-		background-color: rgba(22,26,30,0.6);
-		color: rgb(195, 212, 232);
 		display: inline-block;
 		font-size: 0.825rem;
-		padding: 0.25rem 0.5rem;
-		text-transform: uppercase;
+		padding: 0.25rem 0;
 	}
 
 	.skill-category-content {
-		color: #C7D2DB;
+		padding-left: 0.5rem;
 		font-size: 0.875rem;
 	}
 }
