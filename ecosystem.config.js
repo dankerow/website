18ed81b7bdf534fb589ec3f0cb5dd9c478aa48dd
Dan <78473524+dankerow@users.noreply.github.com>
2023-03-21 14:16:36 -0700

    build: Updated PM2 deployment configuration

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,10 +1,14 @@
+const env = require('dotenv').config()
+
 module.exports = {
 	apps: [
 		{
 			name: 'danmutombo.com',
-			port: '3000',
 			exec_mode: 'cluster',
 			instances: 'max',
+			env: {
+				...env.parsed
+			},
 			script: './.output/server/index.mjs'
 		}
 	]
