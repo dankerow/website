434686b3805a9a032d909ec9f85c3ead3b438936
Dan <78473524+dankerow@users.noreply.github.com>
2023-04-04 14:10:46 -0700

    fix: vue/no-multiple-template-root

--- src/pages/blog/[...slug].vue
+++ src/pages/blog/[...slug].vue
@@ -5,46 +5,48 @@ const articleDate = formatDate(article.date)
 </script>
 
 <template>
-	<section class="hero d-flex justify-content-center align-items-center flex-column position-relative text-center pt-5">
+	<div>
+		<section class="hero d-flex justify-content-center align-items-center flex-column position-relative text-center pt-5">
+			<div class="container">
+				<h1 class="title m-0 text-uppercase">
+					{{ article.title }}
+				</h1>
+				<p> {{ article.description }}</p>
+
+				<div class="info fs-5 fw-semibold d-flex flex-wrap position-absolute">
+					<div class="row row-cols-auto gx-2">
+						<div class="col">
+							<div v-if="article.date" class="py-1 px-2 rounded-1 text-bg-primary">
+								{{ articleDate }}
+							</div>
+						</div>
+						<div class="col">
+							<div v-if="article.tags" class="py-1 px-2 rounded-1 text-bg-primary text-capitalize">
+								{{ article.tags.join(', ') }}
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
 		<div class="container">
-			<h1 class="title m-0 text-uppercase">
-				{{ article.title }}
-			</h1>
-			<p> {{ article.description }}</p>
-
-			<div class="info fs-5 fw-semibold d-flex flex-wrap position-absolute">
-				<div class="row row-cols-auto gx-2">
-					<div class="col">
-						<div v-if="article.date" class="py-1 px-2 rounded-1 text-bg-primary">
-							{{ articleDate }}
-						</div>
+			<article>
+				<div
+					class="row row-cols-1 row-cols-md-1 row-cols-lg-2 g-4 justify-content-center py-5"
+				>
+					<div class="col col-lg-9">
+						<ContentRenderer :value="article">
+							<template #empty>
+								<p>No content found.</p>
+							</template>
+						</ContentRenderer>
 					</div>
-					<div class="col">
-						<div v-if="article.tags" class="py-1 px-2 rounded-1 text-bg-primary text-capitalize">
-							{{ article.tags.join(', ') }}
-						</div>
+					<div class="col col-lg-3">
+						<BlogToc :links="article.body.toc.links" />
 					</div>
 				</div>
-			</div>
+			</article>
 		</div>
-	</section>
-
-	<div class="container">
-		<article>
-			<div
-				class="row row-cols-1 row-cols-md-1 row-cols-lg-2 g-4 justify-content-center py-5"
-			>
-				<div class="col col-lg-9">
-					<ContentRenderer :value="article">
-						<template #empty>
-							<p>No content found.</p>
-						</template>
-					</ContentRenderer>
-				</div>
-				<div class="col col-lg-3">
-					<BlogToc :links="article.body.toc.links" />
-				</div>
-			</div>
-		</article>
 	</div>
 </template>
