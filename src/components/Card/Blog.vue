08ac629d41c96b01eb5a9f9f5ff28208b8d6c53e
Dan <78473524+dankerow@users.noreply.github.com>
2023-04-04 13:33:00 -0700

    feat: add blog article preview card

---
+++ src/components/Card/Blog.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+defineProps<{
+	post: {
+		title: string
+		description: string
+		_path: string
+	}
+}>()
+</script>
+
+<template>
+	<div class="card shadow-sm">
+		<div class="card-body min-vh-50">
+			<h4 class="card-title text-truncate mb-4">
+				<span class="align-middle d-inline-block text-white">#blog/</span>
+				<span class="align-middle d-inline-block">{{ post.title }}</span>
+
+				<Icon name="material-symbols:open-in-new" class="open-new float-end" />
+			</h4>
+
+			<p class="card-text">
+				{{ post.description }}
+			</p>
+		</div>
+
+		<NuxtLink
+			:title="post.title"
+			:to="post._path"
+			class="stretched-link"
+		/>
+	</div>
+</template>
+
+<style lang="scss" scoped>
+.card {
+	.open-new {
+		opacity: 0;
+		transition: opacity 0.3s ease-in-out;
+	}
+
+	&:hover {
+		.open-new {
+			opacity: 1;
+		}
+	}
+}
+</style>
