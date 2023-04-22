b16193910a4b746cd03c928cff3badef864c1d06
Dan <78473524+dankerow@users.noreply.github.com>
2023-04-21 20:19:54 -0700

    feat: add personal projects

--- src/components/Sections/Projects.vue
+++ src/components/Sections/Projects.vue
@@ -1,6 +1,23 @@
 <script setup lang="ts">
 import { Ref } from 'vue'
 
+const projects = ref([
+  {
+    name: 'Dankerow',
+    description: 'My personal website.',
+    url: 'https://danmutombo.com/',
+    image: 'dankerow.png',
+    tags: ['Vue', 'Nuxt', 'Bootstrap', 'TypeScript']
+  },
+  {
+    name: 'Adrian Salvador',
+    description: 'My personal website.',
+    url: 'https://salvadoradrian.com/',
+    image: 'adriansalvador.png',
+    tags: ['Vue', 'Nuxt', 'Bootstrap', 'TypeScript']
+  }
+])
+
 const repos: Ref<object[]> = ref([])
 const { data: repositoriesData, pending } = await useLazyFetch('https://api.github.com/users/dankerow/repos?per_page=100', { immediate: process.client, default: () => shallowRef() })
 watch(repositoriesData, (newData) => {
@@ -16,6 +33,21 @@ repos.value = repos.value.sort((a, b) => b?.stargazers_count - a?.stargazers_cou
       <h1 class="h3 section-heading mb-5">
         Projects
       </h1>
+
+      <div class="row row-cols-1 row-cols-lg-2 gy-4">
+        <div
+          v-for="(project, index) in projects"
+          :key="`project-${index}`"
+          class="col"
+        >
+          <CardProject
+            v-bind="project"
+            class="h-100"
+          />
+        </div>
+      </div>
+
+      <hr class="my-5">
 
       <div v-if="pending" class="row row-cols-1 row-cols-lg-3 gy-4">
         <div v-for="i in 6" :key="`skeleton-${i}`" class="col">
