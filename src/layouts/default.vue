53bb05c56ee348fd8955f29d0125d30a722d0403
Dan <danker.twist@gmail.com>
2023-05-31 21:10:54 -0700

    feat: use bootstrap v5.3.0

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -54,7 +54,7 @@ useHead({
   ],
   script: [
     {
-      src: 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0-alpha3/js/bootstrap.bundle.min.js',
+      src: 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.0/js/bootstrap.bundle.min.js',
       defer: true
     }
   ]
