21fddd47d947efd0bbb57bf683b485047b0a47e4
Dan <danker.twist@gmail.com>
2023-12-07 02:19:44 -0500

    feat: add post date to blog card

--- src/components/Card/Blog.vue
+++ src/components/Card/Blog.vue
@@ -1,26 +1,35 @@
 <script setup lang="ts">
-defineProps<{
+const props = defineProps<{
   post: {
     title: string
     description: string
     _path: string
+    date: string
   }
 }>()
+
+const postDate = formatDate(props.post.date)
 </script>
 
 <template>
-  <div class="card shadow-sm">
+  <div class="card hoverable shadow-sm">
     <div class="card-body min-vh-50">
       <h3 class="card-title text-truncate mb-4">
-        <span class="align-middle d-inline-block text-white">#blog/</span>
-        <span class="align-middle d-inline-block">{{ post.title }}</span>
+        <span class="align-middle d-inline-block text-white">{{ post.title }}</span>
 
         <Icon name="material-symbols:open-in-new" class="open-new float-end" size="1.15em" />
       </h3>
 
-      <p class="card-text">
+      <p class="card-text text-truncate">
         {{ post.description }}
       </p>
+    </div>
+
+    <div class="card-footer">
+      <small class="text-muted">
+        <Icon name="ph:calendar-light" class="me-1" />
+        {{ postDate }}
+      </small>
     </div>
 
     <NuxtLink
