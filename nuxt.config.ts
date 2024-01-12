ba6fba5b6ba3392366dc80a41c346eb0b335f9c0
Dan <danker.twist@gmail.com>
2024-01-12 16:58:19 -0500

    feat: use Bootstrap JS without Popper

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -32,7 +32,7 @@ export default defineNuxtConfig({
       ],
       script: [
         {
-          src: 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.2/js/bootstrap.bundle.min.js',
+          src: 'https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.2/js/bootstrap.min.js',
           type: 'text/javascript',
           defer: true
         }
