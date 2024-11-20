e0db1d8c987bb5d3bd8e6b6d619b46e6740daf35
Dan <danker.twist@gmail.com>
2024-11-20 18:06:46 -0500

    feat: redesign blog post page

--- app/pages/blog/[...path].vue
+++ app/pages/blog/[...path].vue
@@ -15,65 +15,68 @@ const articleDate = formatDate(article.value.date)
 </script>
 
 <template>
-  <div>
-    <section class="hero d-flex justify-content-center align-items-center flex-column position-relative text-center pt-12">
-      <div class="container">
-        <h2 class="title m-0 text-uppercase">
-          {{ article.title }}
-        </h2>
-        <p> {{ article.description }}</p>
+  <main class="container pt-12 pb-4">
+    <NuxtLink to="/blog" class="d-inline-flex align-items-center btn btn-link mb-8 ps-0">
+      <Icon
+          name="ph:arrow-left-light"
+          class="me-2"
+      />
 
-        <div class="info fs-5 fw-semibold d-flex flex-wrap position-absolute">
-          <div class="row row-cols-auto gx-2">
-            <div class="col">
-              <div v-if="article.date" class="py-1 px-2 rounded-1 text-bg-primary">
-                {{ articleDate }}
-              </div>
-            </div>
-            <div class="col">
-              <div v-if="article.tags" class="py-1 px-2 rounded-1 text-bg-primary text-capitalize">
-                {{ article.tags.join(', ') }}
-              </div>
-            </div>
+      Back to blog posts
+    </NuxtLink>
+
+    <div class="t">
+      <h1 class="text-white text-uppercase">
+        {{ article.title }}
+      </h1>
+
+      <p class="mb-5">
+        {{ article.description }}
+      </p>
+
+      <div class="row justify-content-between fs-6">
+        <div class="col-auto">
+          <div v-if="article.date" class="d-inline-flex align-items-center py-1 text-body-secondary">
+            <Icon
+              name="solar:calendar-bold-duotone"
+              class="me-2"
+            />
+
+            {{ articleDate }}
+          </div>
+        </div>
+
+        <div class="col-auto">
+          <div v-if="article.tags" class="d-inline-flex align-items-center py-1 text-body-secondary text-capitalize">
+            <Icon
+              name="solar:hashtag-square-bold-duotone"
+              class="me-2"
+            />
+
+            {{ article.tags.join(', ') }}
           </div>
         </div>
       </div>
-    </section>
-
-    <div class="container">
-      <article>
-        <div
-          class="row row-cols-1 row-cols-md-1 row-cols-lg-2 g-4 justify-content-center py-5"
-        >
-          <div class="col col-lg-9 order-2 order-lg-1">
-            <ContentRenderer :value="article">
-              <template #empty>
-                <p>No content found.</p>
-              </template>
-            </ContentRenderer>
-          </div>
-          <div class="col col-lg-3 order-1 order-lg-2">
-            <BlogToc :links="article.body.toc.links" />
-          </div>
-        </div>
-      </article>
     </div>
-  </div>
-</template>
 
-<style lang="scss" scoped>
-.hero {
-  background-color: rgba(38, 38, 38, 0.3);
-  border-bottom: 2px solid rgba(38, 38, 38, 0.8);
-  height: 30vh;
-}
-.title {
-  font-size: 4rem;
-  font-weight: 700;
-  text-transform: uppercase;
-  margin: 0;
-}
-.info {
-  bottom: 10px;
-}
-</style>
+    <hr>
+
+    <article>
+      <div
+        class="row row-cols-1 row-cols-md-1 row-cols-lg-2 g-4 justify-content-center py-5"
+      >
+        <div class="col col-lg-9 order-2 order-lg-1">
+          <ContentRenderer :value="article">
+            <template #empty>
+              <p>No content found.</p>
+            </template>
+          </ContentRenderer>
+        </div>
+
+        <div class="col col-lg-3 order-1 order-lg-2">
+          <BlogToc :links="article.body.toc.links" />
+        </div>
+      </div>
+    </article>
+  </main>
+</template>
