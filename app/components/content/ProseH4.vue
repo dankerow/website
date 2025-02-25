749130e0e942f73fc342ac67f992e5583984e2cb
Dan <danker.twist@gmail.com>
2025-02-24 21:39:39 -0500

    feat: customize nuxt mdc ProseH4 component

---
+++ app/components/content/ProseH4.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { computed, useRuntimeConfig } from '#imports'
+
+const props = defineProps<{ id?: string }>()
+
+const { headings } = useRuntimeConfig().public.mdc
+const generate = computed(() => props.id && ((typeof headings?.anchorLinks === 'boolean' && headings?.anchorLinks === true) || (typeof headings?.anchorLinks === 'object' && headings?.anchorLinks?.h4)))
+</script>
+
+<template>
+  <h4 :id="id">
+    <template v-if="id && generate">
+      <a
+          :href="`#${id}`"
+          class="anchor"
+      >
+        <Icon name="ph:link-duotone" class="octicon octicon-link" size="16px" />
+      </a>
+
+      <slot />
+    </template>
+
+    <slot v-else />
+  </h4>
+</template>
