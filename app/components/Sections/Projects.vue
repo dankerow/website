4e3303b6dbb6903d9d110b8a297cdfef6ef9411f
Dan <danker.twist@gmail.com>
2025-02-25 16:05:26 -0500

    refactor: remove github repositories from home page

--- app/components/Sections/Projects.vue
+++ app/components/Sections/Projects.vue
@@ -1,12 +1,4 @@
 <script setup lang="ts">
-interface Repository {
-  name: string
-  language: string
-  stargazers_count: number
-  description: string
-  html_url: string
-}
-
 const projects = [
   {
     name: 'MIA Gospel',
@@ -16,23 +8,6 @@ const projects = [
     tags: ['Vue', 'Nuxt', 'TypeScript']
   }
 ]
-
-const { data: repos, status } = await useLazyFetch('https://api.github.com/users/dankerow/repos?per_page=6', {
-  key: 'repos',
-  default: () => [],
-  transform: (data: Repository[]) => {
-    return data
-      .map((repo) => ({
-        name: repo.name,
-        language: repo.language,
-        stargazers_count: repo.stargazers_count,
-        description: repo.description,
-        html_url: repo.html_url
-      }))
-      .sort((a, b) => b?.stargazers_count - a?.stargazers_count)
-  },
-  deep: false
-})
 </script>
 
 <template>
@@ -56,56 +31,6 @@ const { data: repos, status } = await useLazyFetch('https://api.github.com/users
             v-bind="project"
             class="h-100"
           />
-        </div>
-      </div>
-
-      <hr class="my-5">
-
-      <p class="mb-4 border-start border-3 border-light-subtle ps-2">
-        GitHub repositories sorted by stars.
-      </p>
-
-      <div v-if="status === 'pending'" class="row row-cols-1 row-cols-md-2 row-cols-lg-3 gy-4">
-        <div v-for="i in 6" :key="`skeleton-${i}`" class="col">
-          <div class="card shadow-sm" aria-hidden="true">
-            <div class="card-body placeholder-glow">
-              <h4 class="card-title text-truncate mb-4">
-                <span class="rounded placeholder col-1 me-2" />
-                <span class="rounded placeholder col-2" />
-                <span class="mx-1">/</span>
-                <span class="rounded placeholder col-3" />
-              </h4>
-
-              <p class="card-text">
-                <span class="rounded placeholder placeholder-sm col-7 my-2 d-block" />
-                <span class="rounded placeholder placeholder-sm col-4 my-2 d-block" />
-                <span class="rounded placeholder placeholder-sm col-8 my-2 d-block" />
-              </p>
-            </div>
-
-            <div class="card-footer placeholder-glow">
-              <span class="rounded placeholder col-1" />
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div v-else>
-        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 gy-4">
-          <div
-            v-for="(repo, index) in repos"
-            :key="`repo-${index}`"
-            class="col"
-          >
-            <CardRepository
-              :name="repo.name"
-              :language="repo.language"
-              :stars="repo.stargazers_count"
-              :description="repo.description"
-              :url="repo.html_url"
-              class="h-100"
-            />
-          </div>
         </div>
       </div>
 
