4e2082660eb0a35c2656a5c6b2db97e4f103de1d
Dan <danker.twist@gmail.com>
2025-02-24 21:30:54 -0500

    feat: customize nuxt mdc ProseA component

---
+++ app/components/content/ProseA.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const {
+  href = ''
+} = defineProps<{
+  href: string
+  target: '_blank' | '_parent' | '_self' | '_top' | (string & object) | null | undefined
+}>()
+</script>
+
+<template>
+  <NuxtLink
+    :href="href"
+    :target="target"
+    class="text-decoration-underline link-offset-2 link-offset-3-hover link-underline link-underline-opacity-10 link-underline-opacity-75-hover"
+  >
+    <slot />
+  </NuxtLink>
+</template>
